bdbed6c278edcb4b9f81e2655a3b1ef6ec9680a5
Juan Manuel Bascuñan <70663191+juanmabascu997@users.noreply.github.com>
2023-08-04 12:13:21 -0300

    Update pickingRoutes.js

--- pickingpro-server/routes/pickingRoutes.js
+++ pickingpro-server/routes/pickingRoutes.js
@@ -3,7 +3,7 @@ const { cancelProductsForPick, getProductsToPickByMatch } = require('../controll
 const router = express.Router();
 
 
-router.post('/cancel-products-for-pick', cancelProductsForPick);
+router.get('/cancel-products-for-pick', cancelProductsForPick);
 
 router.get('/products-for-pick-by-match', getProductsToPickByMatch);
 
